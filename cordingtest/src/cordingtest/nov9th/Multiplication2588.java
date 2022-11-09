cd2b43cf1b0534920c970781ff4c9ffdd9c0b062
USER <USER@DESKTOP-P5BOBUL>
2022-11-09 22:34:04 +0900

    곱셈 끝

--- cordingtest/src/cordingtest/nov9th/Multiplication2588.java
+++ cordingtest/src/cordingtest/nov9th/Multiplication2588.java
@@ -1,10 +1,22 @@
 package cordingtest.nov9th;
 
+import java.util.Scanner;
+
 public class Multiplication2588 {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
 
+		int a = sc.nextInt();
+		String b = sc.next();
+		
+		sc.close();
+		
+		System.out.println(a * (b.charAt(2) - '0'));
+		System.out.println(a * (b.charAt(1) - '0'));
+		System.out.println(a * (b.charAt(0) - '0'));
+		System.out.println(a * Integer.parseInt(b));
+		
 	}
 
 }
